afdf5025ca3a613130bfedbcf73cf021cb70c914
Joel <jmartinddam1@gmail.com>
2020-10-13 17:05:55 +0200

    continuado con el metodo facturarCliente

--- Parking.java
+++ Parking.java
@@ -1,4 +1,6 @@
+
 /**
+ * author Joel Martin
  * La clase representa a un parking de una ciudad europea
  * que dispone de dos tarifas de aparcamiento para los clientes
  * que lo usen: la tarifa regular (que incluye una tarifa plana para
@@ -10,31 +12,62 @@
  */
 public class Parking
 {
-    
+    private final char REGULAR = 'R';
+    private final char COMERCIAL= 'C';
+    private final double PRECIO_BASE_REGULAR = 2.0;
+    private final double PRECIO_MEDIA_REGULAR_HASTA11 = 3.0;
+    private final double PRECIO_MEDIA_REGULAR_DESPUES11 = 5.0;
+    private final int HORA_INICIO_ENTRADA_TEMPRANA = 6 * 60;
+    private final int HORA_FIN_ENTRADA_TEMPRANA = 8 * 60 + 30;
+    private final int HORA_INICIO_SALIDA_TEMPRANA = 15 * 60;
+    private final int HORA_FIN_SALIDA_TEMPRANA = 18 * 60;
+    private final double PRECIO_TARIFA_PLANA_REGULAR = 15.0;
+    private final double PRECIO_PRIMERAS3_COMERCIAL = 5.00;
+    private final double PRECIO_MEDIA_COMERCIAL = 3.00;
+    private String nombre;
+    private int cliente;
+    private double importeTotal;
+    private int regular;
+    private int comercial;
+    private int clientesLunes;
+    private int clientesSabado;
+    private int clientesDomingo;
+    private int clienteMaximoComercial; 
+    private double importeMaximoComercial;
+
     /**
      * Inicializa el parking con el nombre indicada por el parámetro.
      * El resto de atributos se inicializan a 0 
      */
-    public Parking() {
-
-       
-
+    public Parking (String queNombre) {
+        nombre = queNombre;
+        cliente = 0;
+        importeTotal = 0;
+        regular = 0;
+        comercial = 0;
+        clientesLunes = 0;
+        clientesSabado = 0;
+        clientesDomingo = 0;
+        clienteMaximoComercial = 0;
+        importeMaximoComercial = 0;
     }
 
     /**
      * accesor para el nombre del parking
      *  
      */
-    public          () {
-         
+    public String getNombre() {
+        return nombre;
+
     }
-    
+
     /**
      * mutador para el nombre del parking
      *  
      */
-    public  () {
-        
+    public void setNombre(String queNombre) {
+        nombre = queNombre;
+
     }
 
     /**
@@ -54,10 +87,12 @@ public class Parking
      *    (leer enunciado del ejercicio)
      */
     public void facturarCliente(char tipoTarifa, int entrada, int salida, int dia) {
-        
-       
-
-        
+        int horasEntrada = entrada / 100;
+        int minutosEntrada = entrada % 100;
+        int horasSalida = salida / 100;
+        int minutosSalida = salida % 100;
+        String s = ":" ;
+        cliente ++;
 
     }
 
@@ -68,17 +103,15 @@ public class Parking
      *  
      */
     public void printEstadísticas() {
-         
+
     }
 
     /**
      *  Calcula y devuelve un String que representa el nombre del día
      *  en el que más clientes han utilizado el parking - "SÁBADO"   "DOMINGO" o  "LUNES"
      */
-    public        diaMayorNumeroClientes() {
-
-        
+    public String diaMayorNumeroClientes() {
+        return nombre;
 
     }
-
 }
